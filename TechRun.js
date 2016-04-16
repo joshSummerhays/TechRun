74a62356f8d029214ed60211ed82485de552a19c
Josh Summerhays <russiansummer@gmail.com>
2016-04-16 10:01:48 -0600

    tech run add

---
+++ TechRun.js
@@ -0,0 +1,17 @@
+//Tech Run
+
+
+
+var hrr16 = {
+	name: 'Daniel Ramos',
+	name: 'Eric Smith',
+  name: 'Nick Randall',
+  name: 'Neekon Etemad',
+  name: 'Mike Cruz',
+  name: 'Jason',
+  name: 'Josh Summerhays',
+  name: 'Timothy Woo',
+  name: 'Chris Weaver',
+  name: 'Leo Vergara'
+
+}
